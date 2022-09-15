7e39a1b6904c30a5fbac2b87cd8ac5c394a14436
kinomant <kinomant@gmail.com>
2022-09-15 16:17:14 +0300

    added task 2 description

---
+++ airflow_workshop/dags/get_data.py
@@ -0,0 +1,49 @@
+import requests
+import pandas as pd
+import pendulum
+import json
+from airflow import DAG
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
+# эти значения надо убрать в Variables
+url = "https://op.itmo.ru/auth/token/login"
+auth_data = {"username": "analytic", "password": "datatest"}
+
+token_txt = requests.post(url, auth_data).text
+token = json.loads(token_txt)["auth_token"]
+headers = {'Content-Type': "application/json", 'Authorization': "Token " + token}
+
+target_fields = ['cop_id_up', 'isu_id_up', 'isu_op_name', 'cop_id_rpd', 'isu_id_rpd', 'cop_disc_name', 'cop_disc_description']
+
+def get_descriptions():
+    df = pd.DataFrame(columns = ['НАШ_ИД_УП', 'ИД_УП', 'ОБРАЗОВАТЕЛЬНАЯ_ПРОГРАММА', 'НАШ_ИД','DISC_DISC_ID', 'ДИСЦИПЛИНА', 'ОПИСАНИЕ'])
+    url_down = 'https://op.itmo.ru/api/record/academic_plan/academic_wp_description/all?format=json&page=1'
+    page = requests.get(url_down, headers=headers)
+    res = json.loads(page.text)['results']
+    for r in res:
+        line = [r['id']]
+        print (r['id'])
+        s = r['academic_plan_in_field_of_study']  
+        line.append (s[0]['ap_isu_id'])
+        line.append (s[0]['title'])
+        descriptions = r["wp_in_academic_plan"]
+        for des in descriptions:
+            linedisc = line.copy()
+            linedisc.append(des['id'])
+            linedisc.append(des['discipline_code'])
+            linedisc.append(des['title'])
+            linedisc.append(des['description'])
+            df.loc[len(df.index)] = linedisc
+    if len(df) > 0:
+        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.wp_description', df.values, target_fields = target_fields)
+
+
+with DAG(dag_id='api_to_stg', start_date=pendulum.datetime(2022, 1, 1, tz="UTC"), schedule_interval="@daily", catchup=False) as dag:
+    t1 = PythonOperator(
+    task_id='get_descriptions',
+    python_callable=get_descriptions
+    )
+ 
+t1 
---
+++ airflow_workshop/sql/stg_ddl.sql
@@ -0,0 +1,2 @@
+create table stg.wp_description
+(id serial, cop_id_up integer, isu_id_up integer, isu_op_name text, cop_id_rpd integer, isu_id_rpd integer, cop_disc_name text, cop_disc_description text);
